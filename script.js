3bc5dab3d34ef42b0edb0609097510583c2c83e6
PierreVelluet <velluetp@gmail.com>
2020-03-10 13:54:41 +0100

    minor fixes on footer and stuffs

--- script.js
+++ script.js
@@ -9,16 +9,18 @@ document.getElementById("addInput").addEventListener('keypress', function (e) {
 })
 
 function addOneComment() {
-    if (document.getElementById("addInput").value != "" && document.getElementsByClassName("comment").length < 6) {
+    if (document.getElementById("addInput").value != "" && document.getElementsByClassName('comment').length < 10) {
       //set selector in variables
       const unorderedList = document.getElementById("list");
       const listItem = document.createElement("li");
+      const spanItem = document.createElement("span");
   
       //add class="comment" to the li
       listItem.classList.add("comment");
       //create a text node with the value of the input, then append it to the li
       const textNode = document.createTextNode(document.getElementById("addInput").value);
-      listItem.appendChild(textNode)
+      spanItem.appendChild(textNode)
+      listItem.appendChild(spanItem)
       //append the li to the ul, then clear the input field
       unorderedList.appendChild(listItem);
       document.getElementById("addInput").value = "";
